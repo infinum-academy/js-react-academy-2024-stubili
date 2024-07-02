707955ca88c12aa0ae61a4b28832a6b476f466e0
Luka Stubican <luka.stubican@gmail.com>
2024-07-02 19:08:17 +0200

    Added delete buttons and local storage saving

--- App/index.js
+++ App/index.js
@@ -1,15 +1,21 @@
-let reviewArray = [
-    {
-        reviewText: "Very good movie, actually the best movie ever, perchance",
-        score: 5
-    },
-    {
-        reviewText: "Mind boggling experience, surpassed all expectations",
-        score: 1
-    }
-]
+let reviewArray = [];
+let avgScore = 0.0;
+
+//save the review array to local storage
+function saveToLocalStorage(){
+    const reviewListString = JSON.stringify(reviewArray);
+    localStorage.setItem('reviewList',reviewListString);
+}
+
+//loads the reviews from local storage
+function loadFromLocalStorage(){
+    const reviewListString = localStorage.getItem('reviewList');
+    const reviewList = JSON.parse(reviewListString);
+    return reviewList;
+}
 
 function renderReviews() {
+    reviewArray = loadFromLocalStorage();
     //initial render when booting up the app, displays all the reviews from the array
     for (review of reviewArray){
         createReview(review);
@@ -57,6 +63,9 @@ function addReview() {
     //pushing our object into the array
     reviewArray.push(review);
 
+    //saving our review list to local storage
+    saveToLocalStorage();
+
     //creating the element for the review
     createReview(review);
 
@@ -66,10 +75,14 @@ function addReview() {
 }
 
 function createReview(review) {
+    //update the average score
+    calculateAvgScore();
+
     //creating our elements
     const reviewElement = document.createElement("div");
     const reviewText = document.createElement("span");
     const reviewScore = document.createElement("p");
+    const deleteButton = document.createElement("button");
     
     //give our div the proper styling
     reviewElement.className = "review-element";
@@ -82,9 +95,40 @@ function createReview(review) {
     reviewScore.innerHTML = review.score + "/5";
     reviewElement.appendChild(reviewScore);
 
+    //creating the delete button
+    deleteButton.innerHTML = "Remove";
+    deleteButton.onclick = () => {
+        deleteReview(review,reviewElement)
+    };
+    reviewElement.appendChild(deleteButton);
+
     //putting our review in the proper div
     const reviewDiv = document.getElementById("reviews");
     reviewDiv.appendChild(reviewElement);
+}
+
+//deletes the review and updates the score
+function deleteReview(review,reviewElement) {
+    reviewElement.remove();
+    reviewArray = reviewArray.filter((r) => {
+        return r !== review;
+    });
+    calculateAvgScore();
+    saveToLocalStorage();
+}
+
+//calculates the average score of the movie from all the reviews
+function calculateAvgScore(){
+    if (reviewArray.length == 0){
+        document.getElementById("score").innerHTML = "0.0 / 5";
+        return;
+    }
+    let sum = 0;
+    for (review of reviewArray){
+        sum += parseInt(review.score);
+    }
+    avgScore = sum / reviewArray.length;
+    document.getElementById("score").innerHTML = avgScore.toFixed(1) + " / 5";
 }
 
 renderReviews()
